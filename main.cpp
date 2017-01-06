c04acd570caaba290fbf13875794e3c50d956fe0
Tom <Tom@yunlongs-mbp>
2017-01-05 22:56:43 -0500

    end of 01/05/2017

--- main.cpp
+++ main.cpp
@@ -1,46 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.cpp
- * Author: Tom
- *
- * Created on November 27, 2016, 4:16 PM
- */
-
 #include <cstdlib>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-/*
- * 
- */
 int main(int argc, char** argv) {
     
-    cout<<"Enter a number"<<endl;
-    int i;
-    cin>>i;
-    if(i<0) cout<<"negative";
-    else if(i==0) cout<<"zero";
-    else cout<<"positive";
     
-    cout<<endl;
-    
-    switch(i){
-        case 0 : cout<<"0";break;
-        case -1 : cout<<"-1";break;
-        case 1 : cout<<"1";break;
-        default : cout<<"something else";
-    }
-    
-    cout<<endl;
-    
-    cout<<&i;
 
     return 0;
 }
+
+
+
+class Solution {
+public:
+    
+    // E 461. Hamming Distance
+    int hammingDistance(int x, int y) {
+        int z=x^y, count=0;
+        while(z>0){
+            if(z%2==1) count++; // z%2 to get last digit
+            z/=2;
+        }
+        return count;
+
+        // or return __builtin_popcount(x^y);
+    }
+    
+    // E 412. Fizz Buzz
+    vector<string> fizzBuzz(int n) {
+        
+    }
+};
 
