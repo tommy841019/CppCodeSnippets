f17b7fa7e4640cbe8bc78c76b57a5f558f8007b7
Tom <Tom@yunlongs-mbp>
2017-01-05 23:42:22 -0500

    end of 01/05/2017

--- main.cpp
+++ main.cpp
@@ -5,8 +5,6 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-    
-    
 
     return 0;
 }
@@ -15,6 +13,8 @@ int main(int argc, char** argv) {
 
 class Solution {
 public:
+    
+    
     
     // E 461. Hamming Distance
     int hammingDistance(int x, int y) {
@@ -28,9 +28,42 @@ public:
         // or return __builtin_popcount(x^y);
     }
     
+    
+    
     // E 412. Fizz Buzz
     vector<string> fizzBuzz(int n) {
-        
+        vector<string> v; // should be slower, due to resize vector when push_back
+        for(int i=1; i<=n; ++i){
+            if(i%15==0) v.push_back("FizzBuzz");
+            else if(i%5==0) v.push_back("Buzz");
+            else if(i%3==0) v.push_back("Fizz");
+            else v.push_back(to_string(i));
+        }
+        return v;
+    }
+    vector<string> fizzBuzz_v2(int n) {
+        vector<string> v(n);
+        for(int i=1; i<=n; ++i){
+            if(i%15==0) v[i-1]="FizzBuzz";
+            else if(i%5==0) v[i-1]="Buzz";
+            else if(i%3==0) v[i-1]="Fizz";
+            else v[i-1]=to_string(i);
+        }
+        return v;
+    }
+    
+    
+    
+    // E 344. Reverse String
+    string reverseString(string s) {
+        return string(s.rbegin(), s.rend());
+    }
+    string reverseStringv2(string s) {
+        int i=0, j=s.size()-1;
+        while(i<j){
+            swap(s[i++], s[j--]);
+        }
+        return s;
     }
 };
 
